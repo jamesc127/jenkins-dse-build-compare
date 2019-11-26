c635a8940c7d02f87a0421812caf4ced5d30d777
James Colvin <jamesc127@gmail.com>
2019-11-26 15:25:58 -0600

    changing from main to method with argumnets for Jenkins shared lib

--- src/main/scala/com/datastax/BuildCompare.scala
+++ src/main/scala/com/datastax/BuildCompare.scala
@@ -10,97 +10,96 @@ import scala.util.Try
 object BuildCompare extends App {
   val config      = ConfigFactory.load()
 //  val config      = ConfigFactory.parseFileAnySyntax(new File("./src/main/resources/stdevcheck.json"))
-  val build       = config.getInt("jenkins.build")
   val grafana     = new httpClient
   val bMath       = new BuildMath
   val XmlBuilder  = new jUnitXmlBuilder
   val usejdbc     = config.getBoolean("jdbc.usejdbc")
-  val dse         = new dseSession(new InetSocketAddress(config.getString("dse.host"),config.getInt("dse.port")),!usejdbc)
   val jdbc        = new jdbcSession(config.getString("jdbc.host"),config.getString("jdbc.username"),config.getString("jdbc.password"),usejdbc)
   var metricArray = ArrayBuffer[BigDecimal]()
 
   case class dseInsert(build: Int, time: Long, metric: String, kind: String, value: BigDecimal)
   implicit val formats: DefaultFormats.type = DefaultFormats
   def longToTimestamp(timeInMillis: BigInt)= timeInMillis.toLong * 1000L
+  def runBuildCompare(startTime:Long,endTime:Long,baseline:Int,tolerance:Float,dseHost:String,grafanaHost:String,buildId:Int): Unit = {
+    val queries = XmlBuilder.buildQueryObjects()
+    val build   = buildId
+    val dse     = new dseSession(new InetSocketAddress(dseHost,config.getInt("dse.port")),!usejdbc)
+    for (i <- queries.indices) {
+      //Query Prometheus and create result set
+      val json = parse(
+        grafana.get(s"${config.getString("grafana.scheme")}" +
+          s"$grafanaHost" + ":" +
+          s"${config.getInt("grafana.port").toString}" +
+          s"${config.getString("grafana.path")}" +
+          s"?query=${queries(i).getQuery}" +
+          s"&start=${startTime.toString}" +
+          s"&end=${endTime.toString}" +
+          s"&step=${config.getInt("grafana.step").toString}"))
+      val value = if ((json \\ "values").values.nonEmpty) (json \\ "values").values else Map.empty[String,Any]
+      val results = value("values").asInstanceOf[List[List[Any]]]
 
-  val queries = XmlBuilder.buildQueryObjects()
+      //Insert individual metrics into C* or JDBC from Prometheus
+      for (r <- results) {
+        println(r(1).asInstanceOf[String])
 
-  for (i <- queries.indices) {
-    //Query Prometheus and create result set
-    val json = parse(
-      grafana.get(s"${config.getString("grafana.scheme")}" +
-      s"${config.getString("grafana.host")}" + ":" +
-      s"${config.getInt("grafana.port").toString}" +
-      s"${config.getString("grafana.path")}" +
-      s"?query=${queries(i).getQuery}" +
-      s"&start=${config.getLong("grafana.start-time").toString}" +
-      s"&end=${config.getLong("grafana.end-time").toString}" +
-      s"&step=${config.getInt("grafana.step").toString}"))
-    val value = if ((json \\ "values").values.nonEmpty) (json \\ "values").values else Map.empty[String,Any]
-    val results = value("values").asInstanceOf[List[List[Any]]]
+        Try {
+          metricArray += BigDecimal(r(1).asInstanceOf[String])
 
-    //Insert individual metrics into C* or JDBC from Prometheus
-    for (r <- results) {
-      println(r(1).asInstanceOf[String])
+          val dseObject = dseInsert(
+            build, longToTimestamp(r.head.asInstanceOf[BigInt]), queries(i).getTestCase,
+            "value", BigDecimal(r(1).asInstanceOf[String]).doubleValue())
 
-      Try {
-        metricArray += BigDecimal(r(1).asInstanceOf[String])
-
-        val dseObject = dseInsert(
-          build, longToTimestamp(r.head.asInstanceOf[BigInt]), queries(i).getTestCase,
-          "value", BigDecimal(r(1).asInstanceOf[String]).doubleValue())
-
-        if (usejdbc) jdbc.insertValue(dseObject) else dse.insertValue(dseObject)
+          if (usejdbc) jdbc.insertValue(dseObject) else dse.insertValue(dseObject)
+        }
       }
+
+      //Insert mean and stdev values into C* or JDBC
+      val meanAndStdDev: (BigDecimal,BigDecimal) = bMath.meanStd(metricArray)
+      val insertMeanObject = dseInsert(build, longToTimestamp(endTime),
+        queries(i).getTestCase, "mean", meanAndStdDev._1.doubleValue())
+      val insertStdevObject = dseInsert(build, longToTimestamp(endTime),
+        queries(i).getTestCase, "stdev", meanAndStdDev._2.doubleValue())
+
+      if (usejdbc) jdbc.insertValue(insertMeanObject) else dse.insertValue(insertMeanObject)
+      if (usejdbc) jdbc.insertValue(insertStdevObject) else dse.insertValue(insertStdevObject)
+
+      //Select baseline mean and stdev from C* or JDBC
+      var baselineBuildMean: Double = 0.00
+      var baselineBuildStdev: Double = 0.00
+      try {
+        baselineBuildMean = if (usejdbc) jdbc.selectBaselineBuild(baseline, queries(i).getTestCase, "mean").getDouble(1)
+        else dse.selectBaselineBuild(baseline, queries(i).getTestCase,"mean").one().getDouble("value")
+      } catch { case e: Exception => println("Error retrieving baseline metric "+e.toString) }
+
+      try {
+        baselineBuildStdev = if (usejdbc) jdbc.selectBaselineBuild(baseline, queries(i).getTestCase, "stdev").getDouble(1)
+        else dse.selectBaselineBuild(baseline, queries(i).getTestCase,"stdev").one().getDouble("value")
+      } catch {
+        case e: Exception => println("Error retrieving baseline metric "+e.toString)
+      }
+
+      //Check whether the percentage of values in `metricArray` that are above or below stdev are within tolerance
+      val stdevCheck = bMath.stdevCheck(
+        baselineBuildStdev, baselineBuildMean, metricArray,
+        tolerance, queries(i).getAboveStdev)
+
+      //Prepare the XML document with results from the stdev check
+      queries(i).failure = stdevCheck("failure").asInstanceOf[Boolean]
+      queries(i).value = stdevCheck("value").asInstanceOf[Float]
+
+      //Write the XML document to a file
+      val xmlFile = new File(config.getString("jenkins.xmlfilepath")+queries(i).getTestCase.filterNot((x: Char)=>x.isWhitespace)+".xml")
+      val bw = new BufferedWriter(new FileWriter(xmlFile,false))
+      bw.write(queries(i).toXml().toString())
+      bw.close()
+
+      //Clear metrics before looping again
+      metricArray.clear()
     }
 
-    //Insert mean and stdev values into C* or JDBC
-    val meanAndStdDev: (BigDecimal,BigDecimal) = bMath.meanStd(metricArray)
-    val insertMeanObject = dseInsert(build, longToTimestamp(config.getLong("grafana.end-time")),
-      queries(i).getTestCase, "mean", meanAndStdDev._1.doubleValue())
-    val insertStdevObject = dseInsert(build, longToTimestamp(config.getLong("grafana.end-time")),
-      queries(i).getTestCase, "stdev", meanAndStdDev._2.doubleValue())
-
-    if (usejdbc) jdbc.insertValue(insertMeanObject) else dse.insertValue(insertMeanObject)
-    if (usejdbc) jdbc.insertValue(insertStdevObject) else dse.insertValue(insertStdevObject)
-
-    //Select baseline mean and stdev from C* or JDBC
-    var baselineBuildMean: Double = 0.00
-    var baselineBuildStdev: Double = 0.00
-    try {
-      baselineBuildMean = if (usejdbc) jdbc.selectBaselineBuild(config.getInt("jenkins.baseline"), queries(i).getTestCase, "mean").getDouble(1)
-      else dse.selectBaselineBuild(config.getInt("jenkins.baseline"), queries(i).getTestCase,"mean").one().getDouble("value")
-    } catch { case e: Exception => println("Error retrieving baseline metric "+e.toString) }
-
-    try {
-      baselineBuildStdev = if (usejdbc) jdbc.selectBaselineBuild(config.getInt("jenkins.baseline"), queries(i).getTestCase, "stdev").getDouble(1)
-      else dse.selectBaselineBuild(config.getInt("jenkins.baseline"), queries(i).getTestCase,"stdev").one().getDouble("value")
-    } catch {
-      case e: Exception => println("Error retrieving baseline metric "+e.toString)
-    }
-
-    //Check whether the percentage of values in `metricArray` that are above or below stdev are within tolerance
-    val stdevCheck = bMath.stdevCheck(
-      baselineBuildStdev, baselineBuildMean, metricArray,
-      config.getDouble("jenkins.stdevtolerance"), queries(i).getAboveStdev)
-
-    //Prepare the XML document with results from the stdev check
-    queries(i).failure = stdevCheck("failure").asInstanceOf[Boolean]
-    queries(i).value = stdevCheck("value").asInstanceOf[Float]
-
-    //Write the XML document to a file
-    val xmlFile = new File(config.getString("jenkins.xmlfilepath")+queries(i).getTestCase.filterNot((x: Char)=>x.isWhitespace)+".xml")
-    val bw = new BufferedWriter(new FileWriter(xmlFile,false))
-    bw.write(queries(i).toXml().toString())
-    bw.close()
-
-    //Clear metrics before looping again
-    metricArray.clear()
+    if (usejdbc) jdbc.closeSession() else dse.closeSession()
+    if (usejdbc && jdbc.connection.isClosed) System.exit(0)
+    else if (!usejdbc && dse.session.isClosed) System.exit(0)
+    else System.exit(1)
   }
-
-  if (usejdbc) jdbc.closeSession() else dse.closeSession()
-  if (usejdbc && jdbc.connection.isClosed) System.exit(0)
-  else if (!usejdbc && dse.session.isClosed) System.exit(0)
-  else System.exit(1)
-
 }
